326d974026d1835868bda4e7e03652d1d6f1a85f
1160300527 <15683609851@163.com>
2018-12-22 00:01:59 +0800

    half

--- src/main/java/Location.java
+++ src/main/java/Location.java
@@ -1,49 +1,116 @@
-import java.io.IOException;
+import java.io.Serializable;
 
-public class Location {
-    private double longitude;
-    private double latitude;
+public class Location implements Serializable {
+    private int row;
+    private int col;
+    private int id;
+    private String type;
+    private double longitude ;
+    private double latitude ;
 
-    public Location(double longitude, double latitude) {
+    public Location(String type, double longitude, double latitude, int id) {
+        this.type = type;
+        this.id = id;
         this.longitude = longitude;
+        this.latitude = latitude;
+        setNum();
+    }
+
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+
+    public void setLatitude(double latitude) {
         this.latitude = latitude;
     }
 
-
-    public void _set_longtitude(double longitude) {
-        this.longitude = longitude;
+    public void setId(int id){
+        this.id = id;
     }
 
-
-    public void _set_latitude(double latitude) {
-        this.latitude = latitude;
+    public void setType(String type){
+        this.type = type;
     }
 
-
-    public double _get_longitude() {
+    public double getLongitude() {
         return longitude;
     }
 
 
-    public double _get_latitude() {
+    public double getLatitude() {
         return latitude;
     }
 
+    public String getType() {
+        return type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getArea(){
+        return row*360+col;
+    }
+
+
+    private void setNum(){
+        col = (int)Math.ceil(longitude/0.1);
+        row = (int)Math.ceil(latitude/0.1);
+    }
+
+
     /**
-     * 计算两个位置的距离，测试阶段,为了简化直接算点的直线距离
-     *
+     * 计算两个位置的距离，测试阶段,为了简化,直接根据经纬度计算距离，使用时，可以
+     * 调用百度的API计算（下面函数即实现该功能）
      * @param driver
      * @return
      */
     public double _far_away_from(Location driver) {
-        double diff_longitude = longitude - driver._get_longitude();
-        double diff_latitude = latitude - driver._get_latitude();
-        return Math.sqrt((diff_longitude * diff_longitude +
-                diff_latitude * diff_latitude));
+        double EARTH_RADIUS = 6378137;//赤道半径(单位m)
+
+        double radLat1 = rad(latitude);
+        double radLat2 = rad(driver.getLatitude());
+
+        double radLon1 = rad(longitude);
+        double radLon2 = rad(driver.getLongitude());
+
+        if (radLat1 < 0)
+            radLat1 = Math.PI / 2 + Math.abs(radLat1);// south
+        if (radLat1 > 0)
+            radLat1 = Math.PI / 2 - Math.abs(radLat1);// north
+        if (radLon1 < 0)
+            radLon1 = Math.PI * 2 - Math.abs(radLon1);// west
+        if (radLat2 < 0)
+            radLat2 = Math.PI / 2 + Math.abs(radLat2);// south
+        if (radLat2 > 0)
+            radLat2 = Math.PI / 2 - Math.abs(radLat2);// north
+        if (radLon2 < 0)
+            radLon2 = Math.PI * 2 - Math.abs(radLon2);// west
+        double x1 = EARTH_RADIUS * Math.cos(radLon1) * Math.sin(radLat1);
+        double y1 = EARTH_RADIUS * Math.sin(radLon1) * Math.sin(radLat1);
+        double z1 = EARTH_RADIUS * Math.cos(radLat1);
+
+        double x2 = EARTH_RADIUS * Math.cos(radLon2) * Math.sin(radLat2);
+        double y2 = EARTH_RADIUS * Math.sin(radLon2) * Math.sin(radLat2);
+        double z2 = EARTH_RADIUS * Math.cos(radLat2);
+
+        double d = Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)+ (z1 - z2) * (z1 - z2));
+        //余弦定理求夹角
+        double theta = Math.acos((EARTH_RADIUS * EARTH_RADIUS + EARTH_RADIUS * EARTH_RADIUS - d * d) / (2 * EARTH_RADIUS * EARTH_RADIUS));
+        double dist = theta * EARTH_RADIUS;
+        return dist;
+    }
+
+    private static double rad(double d)
+    {
+        return d * Math.PI / 180.0;
     }
 
 
-    /**
+    /*
      * 计算两个位置的距离，实际使用阶段,可以直接调用百度API计算距离，为方便使用具体实现及
      * 各个类封装在BaiduDistance.jar中。
      * @param driver    司机的位置
@@ -60,6 +127,12 @@ public class Location {
         return new GetDistance().getDistance(from, to);
     }
     */
+
+    @Override
+    public String toString() {
+        //return "type:"+type+" \tid:"+id+" \tlongitude:" + longitude + " \tlatitude:" + latitude;
+        return "Location [type=" + type + ", id=" + id + ", longitude" + longitude + ", latitude"+latitude+"]";
+    }
 
 
 }
--- src/main/java/_receive_info.java
+++ src/main/java/_receive_info.java
@@ -1,26 +1,34 @@
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.*;
 import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.JavaDStream;
 import org.apache.spark.streaming.api.java.JavaPairDStream;
 import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import scala.Tuple2;
 
 public class _receive_info {
+
     public static void main(String args[]){
-        String passenger = "passenger";
-        String driver = "driver";
         SparkConf conf = new SparkConf().setMaster("local[2]").setAppName("online_car-hailing");
         JavaStreamingContext jssc = new JavaStreamingContext(conf, Durations.seconds(1));
         JavaReceiverInputDStream<String> lines = jssc.socketTextStream("localhost", 9999);
         //lines.persist().print();//dstream().saveAsObjectFiles(".//resource/data","obj");
-        JavaPairDStream<String, Location>info = lines.mapToPair(line->{
+        JavaDStream<Location> info = lines.map(line->{
             String value[] = line.split("\\s+");
             double longitude = Double.valueOf(value[1].split(":")[1]);
             double latitude = Double.valueOf(value[2].split(":")[1]);
-            Location loc = new Location(longitude, latitude);
-            return new Tuple2<>(value[0],loc);
+            String typeId[] = value[0].split(":");
+            Location loc = new Location(typeId[0],longitude, latitude,Integer.valueOf(typeId[1]));
+            return loc;
         });
         info.print();
+        //new _receive_info().savePassengerFile(jssc.sparkContext(),info.filter(location -> location.getType().equals("passenger")));
+        //new _receive_info().saveDriverFile(jssc.sparkContext(),info.filter(location -> location.getType().equals("driver")));
+        new _receive_info().readFile(jssc.sparkContext(), "passenger").show();
+        new SQLContext(SparkSession.builder().config(conf).getOrCreate());
         jssc.start();
         try {
             jssc.awaitTermination();
@@ -28,4 +36,43 @@ public class _receive_info {
             e.printStackTrace();
         }
     }
+
+    public void savePassengerFile(JavaSparkContext sc,JavaDStream<Location> dstream){
+        //sc.textFile("//data/passenger.txt").map(.split(",")).map(p => Person(p(0), p(1).trim.toInt)).toDF();
+        dstream.foreachRDD((JavaRDD<Location>rdd)->{
+            if(!rdd.isEmpty()){
+                SparkSession session = SparkSession.builder().config(rdd.context().getConf()).getOrCreate();
+                Dataset<Row> df = session.createDataFrame(rdd, Location.class);
+                df.write().mode(SaveMode.Append).save("passenger-info");
+            }
+        });
+    }
+
+    public void saveDriverFile(JavaSparkContext sc,JavaDStream<Location> dstream){
+        //sc.textFile("//data/passenger.txt").map(.split(",")).map(p => Person(p(0), p(1).trim.toInt)).toDF();
+        dstream.foreachRDD((JavaRDD<Location>rdd)->{
+            if(!rdd.isEmpty()){
+                SparkSession session = SparkSession.builder().config(rdd.context().getConf()).getOrCreate();
+                Dataset<Row> df = session.createDataFrame(rdd, Location.class);
+                df.write().mode(SaveMode.Append).save("driver-info");
+            }
+        });
+    }
+
+
+    public void match(JavaPairDStream<Integer,Location>passenger,JavaPairDStream<Integer,Location>driver){
+        JavaPairDStream<Integer, Tuple2<Location,Location>>areaInfo = passenger.join(driver);
+
+    }
+
+    public Dataset readFile(JavaSparkContext sc, String type){
+        SQLContext sqlContext = new SQLContext(sc);
+        //Dataset dataset = sqlContext.jsonFile("info");
+        Dataset dataset = sqlContext.load("info").toJSON();
+        //dataset.show();
+        //Dataset<Row> dataset = sqlContext.read.format("parquet").load("路径");
+        return  dataset;
+    }
+
 }
+
